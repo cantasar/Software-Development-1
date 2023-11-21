2602a82a1c9dcd432920d38ec3d29c18ccd18eb0
cantasar <62062865+cantasar@users.noreply.github.com>
2023-11-21 21:39:03 +0300

    update

--- introduction-to-software-development-1/midterm/Q2-sum_nums.c
+++ introduction-to-software-development-1/midterm/Q2-sum_nums.c
@@ -9,9 +9,9 @@ int main()
 	for (int i = 0; i < 100; i++)
 	{
 		scanf("%d", &num);
-		if (num < 30)
+		if (i < 30)
 			sum1 = sum1 + i;
-		if (num >= 30 && num < 60)
+		if (i >= 30 && i < 60)
 			sum2 = sum2 + i;
 	}
 	printf("total value 0-29: %d", sum1);
