84baf39e41d8e6c98f6d2af423390cdd133a840f
cantasar <62062865+cantasar@users.noreply.github.com>
2023-12-20 10:46:07 +0300

    update

---
+++ INTRODUCTION TO SOFTWARE DEVELOPMENT I/week-12/len_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[100] = {0};
+	printf("enter a string: ");
+	scanf("%s", a);
+
+	int count = 0;
+	char *p = &a[0];
+
+	while (*p != 0)
+	{
+		p++;
+		count++;
+	}
+	printf("len of string: %d \n", count);
+	
+
+	return 0;
+}
